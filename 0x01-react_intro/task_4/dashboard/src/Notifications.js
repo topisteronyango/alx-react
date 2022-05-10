9974ea374fb29a36e98437a5f008c1903082273e
topisteronyango <t.onyango@alustudent.com>
2022-05-10 09:39:05 +0000

    deploy gh-pages

---
+++ 0x01-react_intro/task_4/dashboard/src/Notifications.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Notifications.css';
+import closeIcon from './close-icon.png'
+import { getLatestNotification } from './utils';
+
+const Notifications = () => {
+    return (
+        <div className="Notifications">
+            <p style={{ paddingLeft: '0.5rem' }}>
+            Here is the list of notifications
+            </p>
+            <ul>
+                <li data='default'>New course available</li>
+                <li data='urgent'>New resume available</li>
+                <li data='urgent'
+                dangerouslySetInnerHTML = {{ __html: getLatestNotification() }}></li>
+            </ul>
+            <button style={{position:'absolute', right: '0.5rem' , top: '0.5rem', 
+            border: 'none' , margin: '0.5rem', backgroundColor: 'transparent'}}
+            aria-label = 'close'
+            onClick={console.log('Close button has been clicked')}>
+                <img src={closeIcon} alt='Close Button' 
+                style={{width:'1rem', height: '1rem' , padding: '0.2rem'}}/>
+            </button>
+        </div>
+    );
+};
+
+export default Notifications;
