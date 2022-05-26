cf08d793b475b057940b16391583b2e691a285c0
topisteronyango <t.onyango@alustudent.com>
2022-05-26 10:44:52 -0700

    Converting into an Immutable object using fromJS

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,4 @@
-// import { fromJS } from 'immutable';
-import { fromJS } from './node_modules/immutable/dist/immutable';
+import { fromJS } from "immutable";
 
 const getImmutableObject = (object) => fromJS(object);
 
